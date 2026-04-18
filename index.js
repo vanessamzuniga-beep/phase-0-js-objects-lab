5680779547c1c635a4b4011697794f8155f7f8d9
vanessamzuniga-beep <vanessamzuniga@gmail.com>
2026-04-18 14:34:07 -0500

    Managing event attendees lab

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
 //Write your code here
+// Declare and initialize an attendee object
+let attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+}
+
+// Create a function to log attendee name
+function logAttendeeName(attendee){
+  console.log(attendee.name)
+}
+
+// Create a function to log ticket price
+function logTicketPrice(attendee){
+  console.log(attendee.ticketPrice)
+}
+
+// Create a function to update ticket type
+function updateTicketType(attendee, ticketType){
+  attendee.ticketType = ticketType
+}
+
+// Create function to update ticket price
+function updateTicketPrice(attendee, ticketPrice){
+  attendee.ticketPrice = ticketPrice
+}
+
+// Create a function to remove the event property
+function removeEventProperty(attendee){
+  delete attendee.event
+}
+
+// Create a function to add a checked-in property
+function addCheckedInProperty(attendee){
+  attendee.checkedIn = true
+}
 
 
 
